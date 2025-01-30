9e803c985577a38a8ddd7777a96a330fb10f8f77
Kriddie Whitmore <whitmore.kriddie@gmail.com>
2025-01-30 11:49:43 -0500

    test push

--- RScripts_scratch/synoppaper_Q.R
+++ RScripts_scratch/synoppaper_Q.R
@@ -21,38 +21,43 @@ stn06_Q <- rbind(WL_06_subset_july6,WL_06_subset_july7,WL_06_subset_july9)
 
 stn05_Q_mean <- mean(stn05_Q$Q_m3s)
 stn06_Q_mean <- mean(stn06_Q$Q_m3s)
+# 0.07554892
 
 #gavi
 
-WL_01_subset_july18 <- WL_01%>%filter(DateTime > as.POSIXct("2021-07-18 11:00:00") &
-                                       DateTime < as.POSIXct("2021-07-18 15:00:00"))
-WL_01_subset_july22 <- WL_01%>%filter(DateTime > as.POSIXct("2021-07-22 11:00:00") &
-                                       DateTime < as.POSIXct("2021-07-22 15:00:00"))
+WL_01_subset_june18 <- WL_01%>%filter(DateTime > as.POSIXct("2021-06-18 11:00:00") &
+                                       DateTime < as.POSIXct("2021-06-18 15:00:00"))
+WL_01_subset_june22 <- WL_01%>%filter(DateTime > as.POSIXct("2021-06-22 11:00:00") &
+                                       DateTime < as.POSIXct("2021-06-22 15:00:00"))
 
-WL_02_subset_july18 <- WL_02%>%filter(DateTime > as.POSIXct("2021-07-18 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-18 15:00:00"))
-WL_02_subset_july22 <- WL_02%>%filter(DateTime > as.POSIXct("2021-07-22 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-22 15:00:00"))
+WL_02_subset_june18 <- WL_02%>%filter(DateTime > as.POSIXct("2021-06-18 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-18 15:00:00"))
+WL_02_subset_june22 <- WL_02%>%filter(DateTime > as.POSIXct("2021-06-22 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-22 15:00:00"))
 
-WL_03_subset_july18 <- WL_03%>%filter(DateTime > as.POSIXct("2021-07-18 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-18 15:00:00"))
-WL_03_subset_july22 <- WL_03%>%filter(DateTime > as.POSIXct("2021-07-22 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-22 15:00:00"))
+WL_03_subset_june18 <- WL_03%>%filter(DateTime > as.POSIXct("2021-06-18 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-18 15:00:00"))
+WL_03_subset_june22 <- WL_03%>%filter(DateTime > as.POSIXct("2021-06-22 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-22 15:00:00"))
 
-WL_04_subset_july18 <- WL_04%>%filter(DateTime > as.POSIXct("2021-07-18 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-18 15:00:00"))
-WL_04_subset_july22 <- WL_04%>%filter(DateTime > as.POSIXct("2021-07-22 11:00:00") &
-                                        DateTime < as.POSIXct("2021-07-22 15:00:00"))
+WL_04_subset_june18 <- WL_04%>%filter(DateTime > as.POSIXct("2021-06-18 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-18 15:00:00"))
+WL_04_subset_june22 <- WL_04%>%filter(DateTime > as.POSIXct("2021-06-22 11:00:00") &
+                                        DateTime < as.POSIXct("2021-06-22 15:00:00"))
 
-stn01_Q <- rbind(WL_01_subset_july18,WL_01_subset_july22)
-stn02_Q <- rbind(WL_02_subset_july18,WL_02_subset_july22)
-stn03_Q <- rbind(WL_03_subset_july18,WL_03_subset_july22)
-stn04_Q <- rbind(WL_04_subset_july18,WL_04_subset_july22)
+stn01_Q <- rbind(WL_01_subset_june18,WL_01_subset_june22)
+stn02_Q <- rbind(WL_02_subset_june18,WL_02_subset_june22)
+stn03_Q <- rbind(WL_03_subset_june18,WL_03_subset_june22)
+stn04_Q <- rbind(WL_04_subset_june18,WL_04_subset_june22)
 
 stn01_Q_mean <- mean(stn01_Q$Q_m3s,na.rm = TRUE)
+# 0.02378284
 stn02_Q_mean <- mean(stn02_Q$Q_m3s)
+# 0.02671354
 stn03_Q_mean <- mean(stn03_Q$Q_m3s)
+# 0.02391225
 stn04_Q_mean <- mean(stn04_Q$Q_m3s)
+# 0.01690584
 
 p4 <- plot_ly(WL_04, x = ~DateTime, y = ~Q_m3s, type = 'scatter', mode = 'markers') 
 p1 <- plot_ly(WL_01, x = ~DateTime, y = ~Q_m3s, type = 'scatter', mode = 'markers') 
